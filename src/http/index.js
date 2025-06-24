fbf65b203890cda52a82b22153a458e926454efd
Markiyan Dmyterko <marki.dmyterko@gmail.com>
2025-06-24 16:50:48 +0200

    fixing

--- src/http/index.js
+++ src/http/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export function createClient() {
   return axios.create({
-    baseURL: "http://habit-rabbit-backend-production.up.railway.app",
+    baseURL: "https://habit-rabbit-backend-production.up.railway.app",
     withCredentials: true,
   });
 }
