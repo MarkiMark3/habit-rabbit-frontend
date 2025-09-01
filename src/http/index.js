dbb21d7cfbc05d00726a439d8fce435d39ea9687
Markiyan Dmyterko <marki.dmyterko@gmail.com>
2025-09-01 23:00:19 +0200

    fix

--- src/http/index.js
+++ src/http/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export function createClient() {
   return axios.create({
     // baseURL: 'http://localhost:3002',
-    baseURL: 'https://habit-rabbit-backend.vercel.app',
+    baseURL: 'https://habit-rabbit-backend-rbr0.onrender.com',
     withCredentials: true,
   });
 }
